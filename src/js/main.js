9cf24c31fafaad92c8e58f85386adf460a375892
royschuyler <royschuyler@yahoo.com>
2015-06-02 09:34:59 -0500

    updated main.js

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvanMvbWFpbi5qcyIsInNvdXJjZXNDb250ZW50IjpbXX0=
+angular
+  .module('angularAddresses', [])
+
+  .controller('Main', function ($scope) {
+    $scope.names = [
+      'Ben',
+      'Katy',
+      'Greg',
+      'Carter',
+      'Lucas',
+    ];
+  });
