5886c2d8791ccaca68225fb77c48167a94ebe787
royschuyler <royschuyler@yahoo.com>
2015-06-02 10:53:59 -0500

    made a repeating table

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,14 @@
 angular
   .module('angularAddresses', [])
 
-  .controller('Main', function ($scope) {
-    $scope.names = [
-      'Ben',
-      'Katy',
-      'Greg',
-      'Carter',
-      'Lucas',
+  .controller('Main', function () {
+    var vm = this;
+
+    vm.people = [
+      {name: 'Ben', twitter: '@ben123', phone: '505-505-5050'},
+      {name: 'Dan', twitter: '@dandan', phone: '505-505-5050'},
+      {name: 'Elsa', twitter: '@letitgo', phone: '505-505-5050'},
+      {name: 'Amanda', twitter: '@princessamanda', phone: '505-505-5050'},
+      {name: 'Charity', twitter: '@nonprofit', phone: '505-505-5050'}
     ];
   });
