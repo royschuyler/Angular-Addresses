5f7f99a0cd2ac1d7c76d67367d7e4783aa05c38f
royschuyler <royschuyler@yahoo.com>
2015-06-03 12:26:33 -0500

    added img

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,23 @@ angular
     var vm = this;
 
     vm.people = [
-      {name: 'Ben', twitter: '@ben123', phone: '505-505-5050'},
-      {name: 'Dan', twitter: '@dandan', phone: '505-505-5050'},
-      {name: 'Elsa', twitter: '@letitgo', phone: '505-505-5050'},
-      {name: 'Amanda', twitter: '@princessamanda', phone: '505-505-5050'},
-      {name: 'Charity', twitter: '@nonprofit', phone: '505-505-5050'}
+      {name: 'Ben', twitter: '@ben123', phone: '+1 615 234 5678', photo: 'http://i.imgur.com/fsw9I8V.gif'},
+      {name: 'Dan', twitter: '@dandan', phone: '(615) 234-5678', photo: 'http://i.imgur.com/uTONfVS.jpg'},
+      {name: 'Elsa', twitter: '@letitgo', phone: '615-234-5678', photo: 'http://i.imgur.com/Vo0pllf.jpg'},
+      {name: 'Amanda', twitter: '@princessamanda', phone: '1 615 234 5678', photo: 'http://i.imgur.com/yLCfE5l.jpg'},
+      {name: 'Charity', twitter: '@nonprofit', phone: '615.234.5678', photo: 'http://i.imgur.com/AAuJNDl.jpg'}
     ];
+
+    vm.newPerson = {};
+
+    vm.addNewAddress = function () {
+      vm.people.push(vm.newPerson);
+      vm.newPerson = {};
+    };
+
+    vm.removeAddress = function (person) {
+      var index = vm.people.indexOf(person);
+      vm.people.splice(index, 1);
+    };
+
   });
